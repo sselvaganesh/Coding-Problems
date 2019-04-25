5969fe2aa8b48419cdff3ad4205e9308f73b7b92
SELVA GANESH SUDALAIYANDI <selva.s.ganesh@gmail.com>
2019-04-25 19:00:05 -0400

    Longest Palindrome

---
+++ LongestPalindrome.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<string>
+
+bool isPalindrome(const std::string& str){
+
+
+	for(int i=0, j=str.size()-1; j>i; i++, j--){
+
+		if(str[i]!=str[j]){
+			return false;
+			}
+
+		}
+
+	return true;
+
+}
+
+std::string max(std::string s1, std::string s2){
+
+	return s1.size()>s2.size()?s1:s2;
+}
+
+
+std::string helper(std::string input, unsigned len){
+
+	if(len==1){
+		return input;
+		}
+	else if(isPalindrome(input)){
+		return input;
+		}
+	
+	
+	return max( helper(input.substr(1, len-1), len-1), helper(input.substr(0, len-1), len-1) ) ;
+
+
+}
+
+std::string getLongestPalindrome(const std::string& input){
+
+	return helper(input, input.size());
+
+}
+
+int main(){
+
+	std::string input = "banana";
+
+	std::string output = getLongestPalindrome(input);
+
+	std::cout << "Input: " << input << "\t Output: " << output << std::endl;
+
+}
+
+
